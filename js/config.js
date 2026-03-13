930762330d843d8fc8a8ca03f7911b9afebd47bb
alexandregriffet-cmd <alexandre.griffet@gmail.com>
2026-03-14 00:07:51 +0100

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,13 @@
+window.A4P_CONFIG = {
+  urls: {
+    pmp: "#",
+    cmp: "https://alexandregriffet-cmd.github.io/CMP_academie_performances.html/CMP_academie_performances.html",
+    epe: "#"
+  },
+  labels: {
+    pmp: "PMP",
+    cmp: "CMP",
+    epe: "Équilibre"
+  },
+  storageKey: "a4p_hub_results"
+};
