5efe84e9f948f31d2b06dccdeb103a36e7c689cc
alexandregriffet-cmd <alexandre.griffet@gmail.com>
2026-03-16 00:46:52 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -11,3 +11,12 @@ window.A4P_CONFIG = {
   },
   storageKey: "a4p_hub_results"
 };
+
+/* =========================
+   Configuration Supabase CMP
+   ========================= */
+
+window.CMP_SUPABASE_URL = "https://nhcfoepvairdseojjybi.supabase.co";
+
+window.CMP_SUPABASE_ANON_KEY =
+"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5oY2ZvZXB2YWlyZHNlb2pqeWJpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM1MDc3NzcsImV4cCI6MjA4OTA4Mzc3N30.kPGteGjoZydTlsO0Tes1nh1dOkHoj9vEXVEUw7ny5_I";
